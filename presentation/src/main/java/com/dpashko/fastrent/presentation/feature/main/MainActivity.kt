2baa285f9d97743769cf75d98877ba3f1fe7dd37
Dmytro Pashko <dmytro.pashko@medtronic.com>
2023-02-20 01:50:19 +0200

    Migrated MainActivity to ViewBinding.

--- presentation/src/main/java/com/dpashko/fastrent/presentation/feature/main/MainActivity.kt
+++ presentation/src/main/java/com/dpashko/fastrent/presentation/feature/main/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import com.dpashko.fastrent.R
+import com.dpashko.fastrent.databinding.ActivityMainBinding
 import com.dpashko.fastrent.presentation.navigation.NavigationControllerProvider
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -16,10 +17,12 @@ class MainActivity : AppCompatActivity() {
 
     @Inject
     lateinit var navigationControllerProvider: NavigationControllerProvider
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navigationControllerProvider.setController(navHostFragment.navController)
